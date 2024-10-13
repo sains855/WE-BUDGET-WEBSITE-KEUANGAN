bdbefc3172b3b8712c23d78157165884abbad79f
sains855 <github>
2024-10-14 07:54:05 +0800

    RPL

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Default
+    | Authentication Defaults
     |--------------------------------------------------------------------------
     |
     | This option controls the default authentication "guard" and password
